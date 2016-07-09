ddbf813dd5017742ec3c459367fb467f81cd3d81
Will Johansson <will.johansson@gmail.com>
2016-07-09 14:28:18 -0700

    lol

--- dmxpy/dmx.py
+++ dmxpy/dmx.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
-import serial, sys
+import serial, sys, time
 
 class Dmx:
+    DEFAULT_PORT = "/dev/cu.usbserial-A6YPVITY"
     START_MSG = 0x7E
     LABEL     = 0x06
     START     = 0x00
     END_MSG   = 0xE7
 
-    def __init__(self, port):
+    def __init__(self, port=DEFAULT_PORT):
         self.device = port
 
         try:
@@ -23,6 +24,40 @@ class Dmx:
         packet.append(Dmx.END_MSG)
         packet = map(chr, packet)
         self.port.write(''.join(packet))
+
+    def test(self):
+        blank = [0,0,0,0,0,0,0,0] 
+        red = [100,0,0,255,255,0,0,0]
+        purple = [100,0,0,255,255,0,255,0]
+
+        x = 0
+        while True:
+            command = []
+            for y in range(1,3):
+                command += blank
+            if x % 2 == 0:
+                command += red
+                x += 1
+            else:
+                command += purple
+                x -= 1
+            self.write(command)
+            time.sleep(1)
+
+    def testa(self):
+        purple = [255,0,0,255,255,0,255,0]
+        blank = [0,0,0,0,0,0,0,0]
+        for x in range(1,18):
+            command = []
+            for y in range(1,x):
+                command += blank
+            command += purple
+            self.write(command)
+            print("sent", command, "to address", x)
+            time.sleep(1)
+
+    def clear(self):
+        self.write([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
     def close(self):
         self.port.close()
