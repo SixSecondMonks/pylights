0d6e189adedb3c903c8949599b1efd17d4b5cbd3
James Tranovich <jtranovich@gmail.com>
2016-07-12 19:51:11 -0700

    config: third DMX test

--- dmxpy/rainbow.py
+++ dmxpy/rainbow.py
@@ -4,6 +4,7 @@ from util import create_hue
 class Rainbow(Program):
     def __init__(self):
         self.iteration = 0
+        self.n = 0
 
     def initial_data(self):
         return dict([('intensity', 100), ('speed', 0xff), ('red', 0x0), ('blue', 0x0), ('green', 0xff)])
@@ -11,7 +12,10 @@ class Rainbow(Program):
     def iterative_data(self):
         data = self.initial_data()
         (r, g, b) = create_hue(self.iteration)
-        self.iteration += 1
+        self.n += 1
+        if (self.n % 10) == 0:
+            self.iteration += 1
+        data['intensity'] = 40
         data['red'] = r
         data['green'] = g
         data['blue'] = b
