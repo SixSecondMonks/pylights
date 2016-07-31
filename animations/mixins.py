70cc4294ebbc1fe406a5c95bc49fa73fd369538f
Will Johansson <will.johansson@gmail.com>
2016-07-30 17:12:43 -0700

    mixins

---
+++ animations/mixins.py
@@ -0,0 +1,23 @@
+from bibliopixel.animation import BaseStripAnim, BaseMatrixAnim
+import pyaudio
+
+RATE  = 44100
+CHUNK = 1024
+
+class AudioMixin:
+    def __init__(self):
+        self.volume = 1
+        
+        def get_volume(data, frame_count, time_info, status):
+            self.volume = rms(data, 2)
+            return (data, pyaudio.paContinue)
+
+        self.py_audio = pyaudio.PyAudio()
+        self.py_audio_stream = self.py_audio.open(format=pyaudio.paInt16, channels=1, input_device_index=7, rate=RATE, input=True, frames_per_buffer=CHUNK, stream_callback=get_volume)
+        self.py_audio_stream.start_stream()
+
+class StripAnim(BaseStripAnim, AudioMixin):
+    pass
+
+class MatrixAnim(BaseMatrixAnim, AudioMixin):
+    pass
