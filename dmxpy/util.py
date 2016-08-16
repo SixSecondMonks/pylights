3b2f68882bb6014c67a7084de01d0aeb9c1baf34
Will Johansson <will.johansson@gmail.com>
2016-08-15 18:16:54 -0700

    remove dmxpy dir

--- dmxpy/util.py
+++
@@ -1,8 +0,0 @@
-import colorsys
-
-def create_hue(what):
-    h = (what % 360) / 360.0
-    s = 1
-    v = 1
-    (r, g, b) = colorsys.hsv_to_rgb(h, s, v)
-    return (int(r * 255), int(g * 255), int(b * 255))
