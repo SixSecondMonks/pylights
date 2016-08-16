af0b1b8da1ebfb18dfe530c088a1eb63fe658c76
Will Johansson <will.johansson@gmail.com>
2016-08-15 17:53:56 -0700

    RainbowsMixed

---
+++ animations/RainbowsMixed.py
@@ -0,0 +1,26 @@
+import bibliopixel.colors as colors
+from mixins import StripAnim
+
+class RainbowCycleAudio(StripAnim):
+    """Generate rainbow wheel equally distributed over strip."""
+
+    def __init__(self, led, start=0, end=-1):
+        super(RainbowCycleAudio, self).__init__(led, start, end)
+
+    def step(self, amt = 1):
+        print dir(self)
+        for i in range(self._size):
+            c = colors.hue_helper(i, self._size, self._step)
+            d = self.decibels
+            d -= 68
+            d = max(0, d)
+            percentage = d / 20
+            percentage = min(percentage, 1.0)
+            brightness = int(round(percentage * 255))
+            self._led.setMasterBrightness(brightness)
+            self._led.set(self._start + i, c)
+
+        self._step += amt
+        overflow = self._step - 256
+        if overflow >= 0:
+            self._step = overflow
